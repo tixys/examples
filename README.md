b9c5580067468199aa74d8909a2c44629da908a9
Alexander Günsche <mail@lxg.de>
2018-04-02 20:54:26 +0200

    initial commit, simple search form and meta files

---
+++ README.md
@@ -0,0 +1,9 @@
+# TIXYS client code examples
+
+All code in this repository is MIT licensed, unless declared differently for a particular file or subdirectory.
+
+## Simple search form
+
+The form is intended to be used on client’s websites in order to let customers select their stations and desired travel date in the context of the shop owner’s primary website.
+
+The code consists of simple HTML, JS and CSS code. Feel free to use or modify the code as needed. The example code uses Bootstrap and jQuery. However, these are completely optional; you are free to use other frameworks or even no framework at all.
---
+++ simple-form/form.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    var apiUrl = tx.shopUrl + "api/app.v1/",
+        form = $(".tx"),
+        fromField = form.find(".from select"),
+        toField = form.find(".to select"),
+        dpField = $(".date input").datepicker({ dateFormat: "yy-mm-dd" });
+
+    // fetch departure stations on page load
+
+    $.get({
+        url : apiUrl + "Station.findStartStations",
+        success : function(stations) {
+            fromField.prop("disabled", false);
+            stations.forEach(function(station){
+                fromField.append($("<option>").attr("value", station.id).text(station.name));
+            });
+        }
+    });
+
+    // fetch destination stations when a start station is selected
+
+    fromField.on("change", function(){
+        toField.prop("disabled", true);
+
+        $.get({
+            url : apiUrl + "Station.findDestinationStations?request=" + parseInt(fromField.val(), 10),
+            success : function(stations) {
+                toField
+                    .prop("disabled", !stations.length)
+                    .val("0")
+                    .find("option:not(.placeholder)").remove();
+
+                stations.forEach(function(station){
+                    toField.append($("<option>").attr("value", station.id).text(station.name));
+                });
+            }
+        });
+    });
+
+
+    form.on('submit', function(ev){
+        // instead of submitting the form, we must collect the values and create a redirect.
+        ev.preventDefault();
+
+        var request = {
+            from : parseInt(fromField.val(), 10),
+            to : parseInt(toField.val(), 10),
+            days: [ dpField.val() ] // yes, this has to be an array
+        };
+
+        try {
+            if (!request.from)
+                throw new Error("Please select a departure station.");
+
+            if (!request.to)
+                throw new Error("Please select a destination station.");
+
+            if (!request.days[0])
+                throw new Error("Please select a date.");
+
+            // redirect to Tixys shop
+            location.href = tx.shopUrl + "#!?" + JSON.stringify(request);
+        }
+        catch (error)
+        {
+            window.alert(error.message);
+        }
+    });
+});
